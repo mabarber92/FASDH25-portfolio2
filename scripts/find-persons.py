c56a0db23bd3f011825aaa4193d1418268cde2ef
mabarber92 <mathew.barber.dh@gmail.com>
2025-04-29 21:02:18 +0500

    removed changed made in class

--- scripts/find-persons.py
+++ scripts/find-persons.py
@@ -1,8 +1,10 @@
 # Import libraries
 import re
+import os
+
 
 # Specify the path to the folder with the articles
-articles_path = "./articles/"
+articles_path = "../articles/"
 
 # Assign the list of persons to be found
 person_list = ["Netanyahu", "Biden", "Trump", "Erdogan"]
@@ -10,18 +12,21 @@ person_list = ["Netanyahu", "Biden", "Trump", "Erdogan"]
 # Create a dictionary for storing the counts
 counts = {}
 
-# Get a list of the article file names - how?
-articles = 
+# Get a list of the article file names - how? :
+
 
 # Loop through the articles - for each article search for each person
 for article in articles:
-    with open(article, 'w', encoding='utf-8') as file:
+    article_path = os.path.join(articles_path, article)
+    
+    with open(article_path, 'r', encoding='utf-8') as file:
         text = file.read()
     for person in person_list:
         results = re.findall(person, text)
-
+        count = len(results)
+        
         # Add to count in dictionary - but what if it is not in the dictionary?
-
+        
 # For each article add the number of results to the dictionary - for each person in list
 
 
